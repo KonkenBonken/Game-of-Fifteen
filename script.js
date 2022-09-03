a99c897c1048b16e1a5f3e24d4c4ab6f811b64bb
KonkenBonken <konrad05pettersson@gmail.com>
2022-09-03 15:48:41 +0200

    Added base script

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+const size = 4,
+	{ floor, abs, random } = Math,
+	field = document.querySelector('#field'),
+	cells = field.getElementsByTagName('div'),
+	empty = document.createElement('div');
+
+empty.id = 'empty';
+
+for (const n of [...Array(size ** 2).keys()].slice(1)) {
+	const cell = document.createElement('div');
+	cell.innerHTML = cell.value = n;
+	field.append(cell);
+}
+
+field.append(empty);
+
+function start() {
+}
+
+start();
