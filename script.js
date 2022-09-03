09242c38b20eed03910415a54f8466c03550a35e
KonkenBonken <konrad05pettersson@gmail.com>
2022-09-03 15:49:04 +0200

    Added shuffle function

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ for (const n of [...Array(size ** 2).keys()].slice(1)) {
 
 field.append(empty);
 
+function shuffle() {
+	const elements = [...cells, empty];
+	for (let i = elements.length - 1; i > 0; i--) {
+		const j = floor(random() * (i + 1));
+		[elements[i], elements[j]] = [elements[j], elements[i]];
+	}
+	field.append(...elements);
+}
+
 function start() {
+	shuffle();
 }
 
 start();
